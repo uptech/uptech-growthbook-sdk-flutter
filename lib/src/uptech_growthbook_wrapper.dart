2026267ec579f48e91a0807947610093f0dd7842
Drew De Ponte <cyphactor@gmail.com>
2022-12-05 15:45:53 -0500

    Add initial working implementation
    Add initial working implementation to get basic support for simple
    boolean toggles such that we can start using it to unblock the majority
    of our needs.
    
    The approach is two fold.
    
    1. Create a wrapper around the GrowthBookSDK so that we can manage the
       concepts of an internal instance for automated testing/live use.
    2. Create a stub client for the automated testing use case to internally
       control toggle evaluation to return the values configured in the test
       setup.
    
    ps-id: e7803d06-5a93-4f1d-a89f-21ebe5a90bc5

---
+++ lib/src/uptech_growthbook_wrapper.dart
@@ -0,0 +1,61 @@
+import 'package:growthbook_sdk_flutter/growthbook_sdk_flutter.dart';
+import 'uptech_growthbook_wrapper_test_client.dart';
+
+/// Thin wrapper around the GrowthBookSDK to facilitate both
+/// live client and stubbed out client for use case in
+/// automated testing.
+class UptechGrowthBookWrapper {
+  UptechGrowthBookWrapper({required this.apiKey});
+  final String apiKey;
+  late GrowthBookSDK _client;
+
+  /// Initialize for use in app
+  void init({required String apiKey}) {
+    _client = _createLiveClient(apiKey);
+  }
+
+  /// Initialize for use in automated test suite
+  void initForTests({Map<String, bool>? seeds}) {
+    _client = _createTestClient(seeds: seeds);
+  }
+
+  /// Force a refresh of toggles from the server
+  Future<void> refresh() async {
+    return _client.refresh();
+  }
+
+  /// Check if a feature is on/off
+  bool isOn(String featureId) {
+    return _client.feature(featureId).on!;
+  }
+
+  GrowthBookSDK _createLiveClient(String apiKey) {
+    return GBSDKBuilderApp(
+      apiKey: apiKey,
+      hostURL: 'https://cdn.growthbook.io/',
+      growthBookTrackingCallBack: (gbExperiment, gbExperimentResult) {},
+    ).initialize();
+  }
+
+  GrowthBookSDK _createTestClient({Map<String, bool>? seeds}) {
+    final gbContext = GBContext(
+      apiKey: 'some-garbage-key-because-we-are-not-using-it',
+      hostURL: 'https://cdn.growthbook.io/',
+      trackingCallBack: (gbExperiment, gbExperimentResult) {},
+    );
+    return GrowthBookSDK(
+      context: gbContext,
+      client: UptechGrowthBookWrapperTestClient(seeds: seeds),
+      features: _seedsToGBFeatures(seeds: seeds),
+    );
+  }
+
+  Map<String, GBFeature> _seedsToGBFeatures({Map<String, bool>? seeds}) {
+    if (seeds != null) {
+      return seeds
+          .map((key, value) => MapEntry(key, GBFeature(defaultValue: value)));
+    } else {
+      return {};
+    }
+  }
+}
---
+++ lib/src/uptech_growthbook_wrapper_test_client.dart
@@ -0,0 +1,28 @@
+import 'package:growthbook_sdk_flutter/growthbook_sdk_flutter.dart';
+
+/// A client to be stubbed into GrowthBookSDK to return explicitly
+/// configured toggle states for automated tests.
+class UptechGrowthBookWrapperTestClient extends BaseClient {
+  UptechGrowthBookWrapperTestClient({this.seeds});
+
+  final Map<String, bool>? seeds;
+
+  @override
+  consumeGetRequest(String path, OnSuccess onSuccess, OnError onError) async {
+    final Map<String, dynamic> data = {
+      'status': 200,
+      'features': _seedsToHashFeatures(seeds),
+      'dateUpdated': DateTime.now(),
+    };
+    onSuccess(data);
+  }
+
+  Map<String, dynamic> _seedsToHashFeatures(Map<String, bool>? seeds) {
+    final Map<String, dynamic> emptyFeatures = {};
+    if (seeds != null) {
+      return seeds.map((key, value) => MapEntry(key, {'defaultValue': value}));
+    } else {
+      return emptyFeatures;
+    }
+  }
+}
