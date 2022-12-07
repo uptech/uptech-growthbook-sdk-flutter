8ae691f9d9cbd84bbf481e9c77b1bf808b1dd252
Andy <andyjhorn@gmail.com>
2022-12-07 12:31:45 -0600

    Add simple tests
    Add some simple tests to ensure the `isOn` method works properly with
    provided and null values.
    
    ps-id: 3ff40089-0d18-4ad3-8444-46d72544de4d

--- test/uptech_growthbook_sdk_flutter_test.dart
+++ test/uptech_growthbook_sdk_flutter_test.dart
@@ -1,14 +1,36 @@
 import 'package:uptech_growthbook_sdk_flutter/uptech_growthbook_sdk_flutter.dart';
 import 'package:test/test.dart';
 
+class ToglTest extends UptechGrowthBookWrapper {
+  ToglTest() : super(apiKey: 'dummy-api-key');
+
+  static ToglTest instance = ToglTest();
+}
+
 void main() {
-  group('A group of tests', () {
-    setUp(() {
-      // Additional setup goes here.
+  group('UpTechGrowthBookSDKFlutter', () {
+    group('when an overridden value is present', () {
+      setUp(() {
+        ToglTest.instance.initForTests(seeds: {
+          'test-value': true,
+        });
+      });
+
+      test('it returns the overridden value', () {
+        expect(ToglTest.instance.isOn('test-value'), isTrue);
+      });
     });
 
-    test('First Test', () {
-      expect(true, isTrue);
+    group('when an overridden value is not present', () {
+      setUp(() {
+        ToglTest.instance.initForTests(seeds: {
+          'some-other-value': true,
+        });
+      });
+
+      test('it returns false', () {
+        expect(ToglTest.instance.isOn('test-value'), isFalse);
+      });
     });
   });
 }
