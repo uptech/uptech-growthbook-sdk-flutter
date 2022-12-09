82521aa584e202cea270d695359bb711c99839a7
Drew De Ponte <cyphactor@gmail.com>
2022-12-08 19:04:40 -0500

    Fix bug where LiveClient path was using test client
    [changelog]
    fixed: bug where live client path was using the test client
    
    ps-id: 821ae3d4-404a-45ab-865a-e0164e461bed

--- lib/src/uptech_growthbook_wrapper.dart
+++ lib/src/uptech_growthbook_wrapper.dart
@@ -43,7 +43,6 @@ class UptechGrowthBookWrapper {
     );
     return GrowthBookSDK(
       context: gbContext,
-      client: UptechGrowthBookWrapperTestClient(seeds: seeds),
       features: _seedsToGBFeatures(seeds: seeds),
     );
   }
